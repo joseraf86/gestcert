083438444b6a8b74bcba75776bcfcaca576393ac
Jose Guevara <jose@18feats.com>
2014-09-16 00:06:45 -0430

    Vistas de Devise agregadas adentro del proyecto

--- app/controllers/certificados_controller.rb
+++ app/controllers/certificados_controller.rb
@@ -1,5 +1,6 @@
 class CertificadosController < ApplicationController
   before_action :set_certificado, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
 
   # GET /certificados
   # GET /certificados.json
