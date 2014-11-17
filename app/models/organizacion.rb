049b2196d34195b46dd94506bf84f1f086f37fa8
ECI <eci@example.com>
2014-11-17 09:43:13 -0430

    Warning por constante con valor cambiado corregido

--- app/models/organizacion.rb
+++ app/models/organizacion.rb
@@ -6,7 +6,6 @@ class Organizacion < ActiveRecord::Base
   validates :nombre,
             length: { in: 1..255 }
 
-  ALPHANUMERIC_REGEX = /\A[-a-zA-Z0-9]+\z/
   validates :rif,
             format: { with: ALPHANUMERIC_REGEX },
             length: { in: 1..255 }
